62be6f885ab2bb67a8ae0c64656f20779348c580
Sari Lautanala <sari.lautanala@helsinki.fi>
2020-07-29 21:25:34 +0300

    -

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 # Front page
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register.html")
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # tarkista, että käyttäjä löytyy
+    session["username"] = username
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
