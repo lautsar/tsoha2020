7e146f06b1c562292487df99b757f2a7d42a32b8
Sari Lautanala <sari.lautanala@helsinki.fi>
2020-08-23 21:37:28 +0300

    Added references.

--- schema.sql
+++ schema.sql
@@ -1,32 +1,31 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT,
-    name TEXT,
-    email TEXT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
     level INTEGER,
     confirmed BOOLEAN,
-    teacher BOOLEAN,
-    admin BOOLEAN
+    role INTEGER
 );
 
 CREATE TABLE lessons (
     id SERIAL PRIMARY KEY,
     level INTEGER,
-    max INTEGER,
-    date DATE,
-    time INTEGER,
-    active BOOLEAN
+    max INTEGER NOT NULL,
+    date DATE NOT NULL,
+    time INTEGER NOT NULL,
+    teacher_id INTEGER REFERENCES users(id)
 );
 
 CREATE TABLE cards (
     id SERIAL PRIMARY KEY,
     times INTEGER,
-    valid DATE,
-    user_id INTEGER
+    bougth DATE,
+    user_id INTEGER REFERENCES users(id)
 );
 
 CREATE TABLE users_lessons (
-    user_id INTEGER,
-    lesson_id INTEGER
+    user_id INTEGER REFERENCES users(id),
+    lesson_id INTEGER REFERENCES lessons(id)
 );
